6aef0658b7122963f26eb21ff0ebd050cbd4c835
Nikhil Kumar Sharma <106933210+nikhilsharma31@users.noreply.github.com>
2025-08-25 00:36:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/PersonalWebsite">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
