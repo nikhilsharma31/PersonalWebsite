19eb755fc6a8ced8df1d981c29dac8d652b0a2d4
Nikhil Kumar Sharma <106933210+nikhilsharma31@users.noreply.github.com>
2025-08-25 00:33:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [react()],
   base: '/PersonalWebsite/',
   server: {
     host: "::",
